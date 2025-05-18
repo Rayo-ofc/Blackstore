a583f0d74e7cadf51866a591e84687d1b7a3a7f1
Rayo-ofc <Iv5nfranco24@gmail.com>
2025-05-18 11:36:44 -0300

    Actualizar script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //creando
+const botonesAgregarCarrito = document.querySelectorAll('.agregar-carrito');
+
+botonesAgregarCarrito.forEach((boton) => {
+  boton.addEventListener('click', () => {
+    const nombreProducto = boton.getAttribute('data-nombre');
+    const precioProducto = boton.getAttribute('data-precio');
+    alert(`Se ha agregado ${nombreProducto} al carrito. Precio: $${precioProducto}`);
+  });
+});
