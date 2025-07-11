549c34ac926b47e2b9ce190159bd2527dcdf67a5
Rayo-ofc <Iv5nfranco24@gmail.com>
2025-07-11 15:45:47 -0300

    Delete api directory

--- api/usuarioModel.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose'
-
-const usuarioSchema = new mongoose.Schema({
-  nombre: String,
-  email: { type: String, unique: true },
-  contraseña: String,
-});
-
-export default mongoose.models.Usuario || mongoose.model('Usuario', usuarioSchema);
